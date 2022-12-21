1a60e63b3aed3dd564d3374b63d251138321ed21
Victor Ledur <victorlledur@gmail.com>
2022-12-20 22:26:17 -0300

    Feat: rating routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,14 +4,15 @@ import userRoutes from "./user";
 import ownerRoutes from "./owner";
 import authentication from "./authentication";
 import recipeRoutes from "./recipe"
+import ratingRoutes from "./rating"
 
 const routes = Router();
 
 routes.use(placeRoutes);
 routes.use(userRoutes);
-
-
-routes.use(ownerRoutes)
-routes.use(authentication)
+routes.use(ownerRoutes);
+routes.use(authentication);
 routes.use(recipeRoutes);
+routes.use(ratingRoutes);
+
 export default routes;
