845112230f02b8295cf71c282c768c9ee08852ce
juealberton <juliaealberton@gmail.com>
2023-01-09 19:34:26 -0300

    feat: user controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,28 +1,43 @@
 import { NextFunction, Request, Response } from "express";
 import { prisma } from "../database/index";
-import bcrypt  from "bcrypt"
+import bcrypt from "bcrypt"
 import verifyEmail from "../helpers/emailcheck"
+import decodeAndGenerateToken from '../helpers/decodeAndGenerateToken'
 
 const userController = {
+    async verifyPassword(password: string) {
+        if (password) {
+            const hash = await bcrypt.hash(password, 10)
+            return hash
+        }
+        return password
+    },
 
     async createUser(req: Request, res: Response, next: NextFunction) {
         try {
-            const { name, email, password, image_link, city, state, country} = req.body;
-                
+            const { name, email, password, image_link, city, state, country } = req.body;
+
+            if (await verifyEmail(email))
+                return res.status(400).send({ message: 'This email already exists' })
+
+            const hash = await bcrypt.hash(password, 10)
 
             const newUser = await prisma.user.create({
-                data: {
-                    name: name,
-                    email: email,
-                    password: password,
-                    image_link: image_link,
-                    city: city,
-                    state: state,
-                    country: country,
-                }
-            });
+                data: { ...req.body, password: hash },
+            })
 
-            res.status(201).json(newUser)
+            res.status(201).json({
+                newUser,
+                token: decodeAndGenerateToken.generateToken({
+                    name: newUser.name,
+                    email: newUser.email,
+                    password: newUser.password,
+                    image_link: newUser.image_link,
+                    city: newUser.city,
+                    state: newUser.state,
+                    country: newUser.country,
+                })
+            })
 
         } catch (error) {
             next(error)
@@ -32,6 +47,13 @@ const userController = {
     async listUsers(req: Request, res: Response, next: NextFunction) {
         try {
             const listUsers = await prisma.user.findMany();
+            let newList = [{}];
+
+            for (const item in listUsers) {
+                const { passwordReset, passwordExpired, ...user } = listUsers[item]
+                newList.push(user)
+            } //para não exibir a senha resete e expired
+
             res.status(200).json(listUsers);
         } catch (error) {
             next(error);
@@ -43,17 +65,19 @@ const userController = {
 
             const { id } = req.params;
 
-            const user = await prisma.user.findUnique({
+            const userId = await prisma.user.findUnique({
                 where: {
                     id,
                 }
             });
 
-            if (!user) {
+            if (!userId) {
                 res.status(404).json("User not found")
             };
 
-            res.status(200).json(user)
+            const { passwordReset, passwordExpired, ...user } = userId //para não exibir a senha resete e expired
+
+            return res.status(200).json(user)
 
         } catch (error) {
             next(error)
@@ -64,7 +88,7 @@ const userController = {
     async updateUser(req: Request, res: Response, next: NextFunction) {
         try {
             const { id } = req.params;
-            const { name, email, password, image_link, city, state, country} = req.body;
+            const { name, email, password, image_link, city, state, country } = req.body;
 
             const user = await prisma.user.findUnique({
                 where: {
@@ -75,6 +99,11 @@ const userController = {
             if (!user) {
                 res.status(400).json("User not found")
             };
+
+            if (req.body.email) {
+                if (await verifyEmail(req.body.email))
+                    return res.status(400).send({ message: 'This email already exists' })
+            }
 
             const updated = await prisma.user.update({
                 where: {
@@ -91,7 +120,7 @@ const userController = {
                 },
             });
 
-            res.status(200).json({ result: updated})
+            res.status(200).json({ result: updated })
         } catch (error) {
             next(error)
 
@@ -109,7 +138,7 @@ const userController = {
             });
 
             if (!user) {
-                res.status(404).json("User not found")
+                return res.status(404).json("User not found")
             };
 
             await prisma.user.delete({
@@ -125,7 +154,7 @@ const userController = {
         }
     },
 
-    
+
 }
 
 export default userController
